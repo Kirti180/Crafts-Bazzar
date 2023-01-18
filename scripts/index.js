b5b450f42c98b0537141a8366c7a7961fa9b746b
sarimkhan208 <sarimkhan7275@gmail.com>
2023-01-18 13:19:40 +0530

    srm done index page

--- scripts/index.js
+++ scripts/index.js
@@ -15,3 +15,25 @@ setInterval(function(){
   index++;
 },3000)
 
+// BURGER SECTION
+
+
+let ham = document.getElementById("hamburger");
+ham.addEventListener("click",function(){
+  this.classList.toggle("is-active");
+  if (this.classList.contains('is-active')){
+    openNav();
+  } else {
+    closeNav();
+  }
+
+})
+function openNav(){
+  console.log("open");
+  document.getElementById("burger").style.display = "initial";
+  document.getElementById("burger").style.transition = "transform 250ms ease-in-out"
+}
+function closeNav(){
+  console.log("close nav")
+  document.getElementById("burger").style.display = "none";
+}
