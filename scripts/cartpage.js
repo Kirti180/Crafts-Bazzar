16b6b5e4d63d1e4c1750b7ec354976cea0ad1646
rekharajput <rekharajput737@gmail.com>
2023-01-19 23:07:20 +0530

    cartpage

--- scripts/cartpage.js
+++ scripts/cartpage.js
@@ -12,7 +12,7 @@
           { 
             localStorage.clear();
             window.location.href="./cartpage.html";
-
+           
           })
      let cartdata=JSON.parse(localStorage.getItem("cart"));
           if(cartdata===null)
@@ -31,16 +31,33 @@
           let card=document.createElement("div");
           let image=document.createElement("img");
           image.setAttribute("src",element.image);
-          let name=document.createElement("h2");
-          name.innerText=element.name;
+          let title=document.createElement("h2");
+          title.innerText=element.title;
           let price1=document.createElement("h3");
           price1.innerText="Price";
           let price=document.createElement("h3");
-          price.innerText="$"+element.price;
+          price.innerText="Rs."+element.price;
           let desc=document.createElement("h3");
           desc.innerText=element.desc;
+          let span = document.createElement("span")
+          let plus=document.createElement("button");
+           plus.innerText="+";
+           plus.addEventListener("click",()=>{
+            //console.log(element.quantity);
+            element.quantity++;
+            //console.log(item.quantity);
+            span.innerText = element.quantity
+            showtotal()
+        })
+           let minus=document.createElement("button");
+           minus.innerText="-";
+           minus.addEventListener("click",()=>{
+                        element.quantity--;
+                        span.innerText = element.quantity
+                        showtotal()
+                    })
           //let del=document.createElement("button");
-          let quantity=document.createElement("h3");
+          // let quantity=document.createElement("h3");
           //del.innerText="Delete";
           // del.addEventListener("click",() =>
           // {
@@ -50,7 +67,79 @@
            localStorage.setItem("checkout",JSON.stringify(cartdata));
           //   window.location.href="./cart.html";
           // })
-          card.append(image,name,price1,price,desc);
+          card.append(image,title,price1,price,desc,plus,minus,span);
           cart.append(card);
         });
       }
+       let total = document.getElementById("order-total")
+      //  let input = document.getElementById("input")
+      // let btn = document.getElementById("btn")
+      // let body = document.getElementById("order-container")
+         let data = JSON.parse(localStorage.getItem("buy"))||[]
+
+// function show(){
+//     body.innerHTML = null
+
+//     data.forEach((item,index)=>{
+//         let div = document.createElement("div")
+//         let img = document.createElement("img")
+//         let h3 = document. createElement("h3")
+//         let des = document.createElement("p")
+//         let rate = document.createElement("p")
+//         let btn = document.createElement("button")
+//         let add = document.createElement("button")
+//         let less = document.createElement("button")
+//         let span = document.createElement("span")
+
+//         img.setAttribute("src",item.image)
+//         h3.innerText = item.title
+//         des.innerText = item.description
+//         rate.innerText = item.price
+//         btn.innerText = "Cancel"
+//         span.innerText = item.quanity
+//         add.innerText = "+"
+//         less.innerText = "-"
+
+//         plus.addEventListener("click",()=>{
+//             item.quanity++
+//             span.innerText = item.quanity
+//             showtotal()
+//         })
+//         minus.addEventListener("click",()=>{
+//             item.quanity--
+//             span.innerText = item.quanity
+//             showtotal()
+//         })
+
+//         // btn.addEventListener("click",()=>{
+//         //   data.splice(index,1)
+//         //   localStorage.setItem("buy",JSON.stringify(data))
+//         //   show()
+//         //   showtotal()
+            
+//         })
+
+        
+//     })
+// }
+// show()
+// console.log(data)
+let sum;
+function showtotal(){
+    sum = 0
+    for(let key of data){
+        sum += key.price*key.quantity
+    }
+    total.innerText=sum 
+    
+}
+      showtotal()
+// btn.addEventListener("click",()=>{
+//     let result = input.value
+
+//     if(result == "Masai30"){
+//         temp = sum*0.3
+//         total.innerText = sum-temp
+//     }
+//})
+      
