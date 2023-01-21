dc3163280e3e2367daa7c67c0321d62faf1150d0
rekharajput <rekharajput737@gmail.com>
2023-01-21 20:27:33 +0530

    completed

--- scripts/cartpage.js
+++ scripts/cartpage.js
@@ -2,18 +2,20 @@
     let cart=document.getElementById("Cart");
     let empty=document.getElementById("submit")
     let buy=document.getElementById("submit")
+    let final_buy=document.getElementById("final_buy")
+       final_buy.addEventListener("click",()=>
+       {
+        // localStorage.setItem("checkout1",JSON.stringify(cartdata));
+        localStorage.setItem("price",total_price)
+        // window.location.href="./checkout.html";
+       })
     empty.addEventListener("click",() =>
           { 
             localStorage.clear();
             window.location.href="./cartpage.html";
 
           })
-          buy.addEventListener("click",() =>
-          { 
-            localStorage.clear();
-            window.location.href="./cartpage.html";
-           
-          })
+         
      let cartdata=JSON.parse(localStorage.getItem("cart"));
           if(cartdata===null)
           {
@@ -22,54 +24,82 @@
           }
 
           displayproduct(cartdata);
-
+let total_price;
           function displayproduct(data)
-      {
+         {
         cart.innerHTML=null;
-        data.forEach((element)=>
-        {
-          let card=document.createElement("div");
-          let image=document.createElement("img");
-          image.setAttribute("src",element.image);
-          let title=document.createElement("h2");
-          title.innerText=element.title;
-          let price1=document.createElement("h3");
+        let cartItemsfromLS=JSON.parse(localStorage.getItem("cart"));
+        console.log(cartItemsfromLS);
+    
+         let total_price=cartItemsfromLS.reduce(function(acc,el){
+            return acc+el.price;
+         },0);
+        console.log(total_price)
+        localStorage.setItem("price1",total_price)
+         document.querySelector("h2>span").innerText=total_price
+        
+    
+         document.querySelector("h1>span").innerText=cartItemsfromLS.length;
+    
+        cartItemsfromLS.forEach(function(el,i){
+           let card=document.createElement("div")
+    
+           let productImg=document.createElement("img")
+           productImg.src=el.image;
+    
+           let title=document.createElement("h3")
+           title.innerText=el.title;
+    
+           let price1=document.createElement("h4");
           price1.innerText="Price";
-          let price=document.createElement("h3");
-          price.innerText="Rs."+element.price;
-          let desc=document.createElement("h3");
-          desc.innerText=element.desc;
-          let span = document.createElement("span")
-          let plus=document.createElement("button");
-           plus.innerText="+";
-           plus.addEventListener("click",()=>{
-            //console.log(element.quantity);
-            element.quantity++;
-            //console.log(item.quantity);
-            span.innerText = element.quantity
-            showtotal()
-        })
-           let minus=document.createElement("button");
-           minus.innerText="-";
-           minus.addEventListener("click",()=>{
-                        element.quantity--;
-                        span.innerText = element.quantity
+          let price=document.createElement("h5");
+          price.innerText="Rs."+el.price;
+           let span = document.createElement("span")
+           let plusbtn=document.createElement("button")
+           plusbtn.innerText="+";
+           plusbtn.addEventListener("click",()=>{
+                       el.quanity++
+                        span.innerText = el.quanity
                         showtotal()
                     })
-          //let del=document.createElement("button");
-          // let quantity=document.createElement("h3");
-          //del.innerText="Delete";
-          // del.addEventListener("click",() =>
-          // {
-          //   let cartdata = JSON.parse(localStorage.getItem("cart"));
-          //   if(cartdata===null) cartdata=[]; 
-          //   cartdata.splice(element)
+    
+           let minusbtn=document.createElement("button")
+           minusbtn.innerText="-";
+           minusbtn.addEventListener("click",()=>{
+                        el.quanity--
+                        span.innerText = el.quanity
+                        showtotal()
+                    })
+                    // btn.addEventListener("click",()=>{
+                    //             data.splice(index,1)
+                    //             localStorage.setItem("buy",JSON.stringify(data))
+                    //             show()
+                    //             showtotal()
+                                  
+                    //           })
+    
+           let buy=document.createElement("button")
+           buy.innerText="BUY";
+            buy.addEventListener("click",() =>
+          { 
+           alert("Order Placed Successfully");
+            window.location.href="./checkout.html";
+            localStorage.setItem("checkout",JSON.stringify(cartdata));
+            localStorage.setItem("price",total_price)
+          })
+            
+        
+    
+           card.append(productImg,title,price1,price,plusbtn,buy,minusbtn,span)
+          
+           document.querySelector("#container").append(card)
+    })
            localStorage.setItem("checkout",JSON.stringify(cartdata));
           //   window.location.href="./cart.html";
           // })
-          card.append(image,title,price1,price,desc,plus,minus,span);
-          cart.append(card);
-        });
+        //   card.append(image,title,price1,price,plus,minus,span);
+        //   cart.append(card);
+        // });
       }
        let total = document.getElementById("order-total")
       //  let input = document.getElementById("input")
